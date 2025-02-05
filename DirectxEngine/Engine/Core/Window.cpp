38edac99650111548185e3cf841856cf32f392c1
Chanwoong Bang <bck4044@gmail.com>
2025-02-05 11:44:51 +0900

    feat: main에 있던 window 생성 기능을 클래스화

---
+++ DirectxEngine/Engine/Core/Window.cpp
@@ -0,0 +1,99 @@
+﻿#include "Window.h"
+
+namespace DirectxEngine
+{
+    Window::Window(
+        uint32 width, 
+        uint32 height, 
+        const std::wstring& title, 
+        HINSTANCE instance, 
+        WNDPROC messageProcedure)
+        : width(width), height(height), title(title), instance(instance)
+    {
+        // 창 생성에 필요한 설정 구조체.
+        // 설정 안한 변수엔 Debug모드에선 쓰레기 값, Release에선 모르는 값이 들어간다.
+        // { } 를 통해 0값으로 다 초기화한다.
+        WNDCLASS wc = { };
+
+        // 메세지 처리를 위한 콜백 전달.
+        wc.lpfnWndProc = messageProcedure;        // 윈도우 메세지에 대한 콜백 전달.
+        // 프로그램 주소 전달.
+        wc.hInstance = instance;
+        // 창 생성에 사용할 클래스 이름 전달.
+        wc.lpszClassName = className.c_str();
+
+        // 클래스 등록.
+        if (!RegisterClass(&wc))
+        {
+            // 오류 메세지 확인.
+            auto error = GetLastError();
+
+            // 메세지 출력.
+            // #1. 출력창 이용 (Output).
+            OutputDebugStringA("Failed to register a window class\n");
+
+            // #2. 메세지 박스 이용.
+            MessageBoxA(nullptr, "Failed to register a window class", "Error", MB_OK);
+
+            __debugbreak();
+        }
+
+        // 창 크기 설정.
+        unsigned int positionX = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
+        unsigned int positionY = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
+
+        // 창 크기 조정.
+        RECT rect = { 0, 0, (long)width, (long)height };
+        AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+        // 창 크기 재설정
+        unsigned int windowWidth = rect.right - rect.left;
+        unsigned int windowHeight = rect.bottom - rect.top;
+
+        // 창 생성.
+        // HWND 창의 핸들 포인터 변수.
+        handle = CreateWindow(
+            className.c_str(),                  // Window class
+            title.c_str(),                      // Window text
+            WS_OVERLAPPEDWINDOW,                // Window style
+
+            // Size and position (x, y, w, h)
+            positionX, positionY, windowWidth, windowHeight,
+
+            nullptr,        // Parent window    
+            nullptr,        // Menu
+            instance,       // Instance handle
+            nullptr         // Additional application data
+        );
+
+        if (handle == nullptr)
+        {
+            // 메세지 출력.
+            // #1. 출력창 이용 (Output).
+            OutputDebugStringA("Failed to create a window\n");
+
+            // #2. 메세지 박스 이용.
+            MessageBoxA(nullptr, "Failed to create a window", "Error", MB_OK);
+
+            __debugbreak();
+        }
+
+        // 창 보이기.
+        ShowWindow(handle, SW_SHOW);
+
+        // 창 메세지 업데이트.
+        UpdateWindow(handle);
+    }
+
+    Window::~Window()
+    {
+        // 클래스 등록 해제.
+        UnregisterClass(className.c_str(), instance);
+    }
+
+    void Window::SetWidthHeight(uint32 width, uint32 height)
+    {
+        this->width = width;
+        this->height = height;
+    }
+}
--- DirectxEngine/Engine/main.cpp
+++ DirectxEngine/Engine/main.cpp
@@ -1,4 +1,7 @@
 ﻿#include <Windows.h>
+#include "Core/Window.h"
+
+using namespace DirectxEngine;
 
 // 창 모드로 할 때 메인 함수 (Entry Point).
 // system의 함수와 만든 함수의 스택 쌓이는 구조가 다르다.
@@ -35,85 +38,8 @@ int WINAPI WinMain(
     _In_ int nShowCmd                 
 )
 {
-    // 창 생성.
-    // 창에 사용할 클래스 이름
-    const wchar_t* className = TEXT("Sample Window Class");    // TEXT <- L, 유니코드를 위해.
-
-    // 창 생성에 필요한 설정 구조체.
-    // 설정 안한 변수엔 Debug모드에선 쓰레기 값, Release에선 모르는 값이 들어간다.
-    // { } 를 통해 0값으로 다 초기화한다.
-    WNDCLASS wc = { };
-
-    // 메세지 처리를 위한 콜백 전달.
-    wc.lpfnWndProc = WindowProc;        // 윈도우 메세지에 대한 콜백 전달.
-    // 프로그램 주소 전달.
-    wc.hInstance = hInstance;
-    // 창 생성에 사용할 클래스 이름 전달.
-    wc.lpszClassName = className;
-
-    // 클래스 등록.
-    if (!RegisterClass(&wc))
-    {
-        // 오류 메세지 확인.
-        auto error = GetLastError();
-
-        // 메세지 출력.
-        // #1. 출력창 이용 (Output).
-        OutputDebugStringA("Failed to register a window class\n");
-        
-        // #2. 메세지 박스 이용.
-        MessageBoxA(nullptr, "Failed to register a window class", "Error", MB_OK);
-
-        __debugbreak();
-    }
-
-    // 창 크기 설정.
-    unsigned int width = 1280;
-    unsigned int height = 800;
-    unsigned int positionX = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
-    unsigned int positionY = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
-
-    // 창 크기 조정.
-    RECT rect = { 0, 0, (long)width, (long)height };
-    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
-
-    // 창 크기 재설정
-    unsigned int windowWidth = rect.right - rect.left;
-    unsigned int windowHeight = rect.bottom - rect.top;
-
-    // 창 생성.
-    // HWND 창의 핸들 포인터 변수.
-    HWND hwnd = CreateWindow(
-        className,                          // Window class
-        TEXT("DirectX Engine Demo"),        // Window text
-        WS_OVERLAPPEDWINDOW,                // Window style
-
-        // Size and position (x, y, w, h)
-        positionX, positionY, windowWidth, windowHeight,
-
-        nullptr,        // Parent window    
-        nullptr,        // Menu
-        hInstance,      // Instance handle
-        nullptr         // Additional application data
-    );
-
-    if (hwnd == nullptr)
-    {
-        // 메세지 출력.
-        // #1. 출력창 이용 (Output).
-        OutputDebugStringA("Failed to create a window\n");
-
-        // #2. 메세지 박스 이용.
-        MessageBoxA(nullptr, "Failed to create a window", "Error", MB_OK);
-
-        __debugbreak();
-    }
-
-    // 창 보이기.
-    ShowWindow(hwnd, SW_SHOW);
-
-    // 창 메세지 업데이트.
-    UpdateWindow(hwnd);
+    //  창 생성.
+    Window window(1280, 800, TEXT("Directx Engine Demo"), hInstance, WindowProc);
 
     MSG msg = { };
     while (msg.message != WM_QUIT) 
@@ -121,7 +47,7 @@ int WINAPI WinMain(
         // 창에 메세지가 들어올 때 실행.
         if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) // GetMessage는 메세지가 들어올 때 까지 대기한다.
         {
-            // 메세지 번역
+            // 메세지 번역.
             TranslateMessage(&msg);
 
             // 메세지 전달.
