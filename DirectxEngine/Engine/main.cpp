a45d412c19da0487d4f443b7b60ceba901c0f669
Chanwoong Bang <bck4044@gmail.com>
2025-02-12 09:49:01 +0900

    fix: 창 시스템에서 콘솔로 바꿔  winmain->main으로 변경 / 디버깅 용 콘솔 + 창 사용 가능

--- DirectxEngine/Engine/main.cpp
+++ DirectxEngine/Engine/main.cpp
@@ -1,34 +1,15 @@
 ﻿#include <Windows.h>
 
-#include "Core/Window.h"
-#include "Core/Engine.h"
+#include <iostream>
 
-#include "Render/Renderer.h"
+#include "Core/Engine.h"
 
 using namespace DirectxEngine;
 
-// 창 모드로 할 때 메인 함수 (Entry Point).
-// system의 함수와 만든 함수의 스택 쌓이는 구조가 다르다.
-// system 함수이기 때문에 WINAPI를 설정 __stdcall.
-/*
-WinMain(
-    _In_ HINSTANCE hInstance,           // HINSTANCE: 핸들 값, 구조체 선언, 프로그램에 대한 포인터 값 관리.
-    _In_opt_ HINSTANCE hPrevInstance,   // 안 쓴다.
-    _In_ LPSTR lpCmdLine,               // LPSTR <- char*, 프로그램 실행할 때 파라미터. 안 쓴다.
-    _In_ int nShowCmd                   // 창 모드 실행할 때 옵션. 안쓴다.
-);
-*/
-
-int WINAPI WinMain( 
-    _In_ HINSTANCE hInstance,         
-    _In_opt_ HINSTANCE hPrevInstance, 
-    _In_ LPSTR lpCmdLine,             
-    _In_ int nShowCmd                 
-)
+int main()
 {
-    Engine engine(1280, 800, TEXT("Directx Engine Demo"), hInstance);
+    std::cout << "테스트\n";
 
+    Engine engine(1280, 800, TEXT("Engine Demo"), GetModuleHandle(nullptr));
     engine.Run();
-
-    return 0;
 }
