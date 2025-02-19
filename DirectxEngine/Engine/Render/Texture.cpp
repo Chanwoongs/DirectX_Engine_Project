38d846f1692167f986bacfaa512402c7db953be9
Chanwoong Bang <bck4044@gmail.com>
2025-02-19 12:30:10 +0900

    feat: stbi_image 라이브러리를 통해 텍스처 파일 불러와서, raw data로 가져오기

---
+++ DirectxEngine/Engine/Render/Texture.cpp
@@ -0,0 +1,71 @@
+﻿#include "Texture.h"
+
+#include <iostream>
+
+#include "Core/Engine.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "Library/stbi_image.h"
+
+namespace DirectxEngine
+{
+    Texture::Texture()
+    {
+    }
+
+    Texture::Texture(const std::string& name, BindType bindType, uint32 index)
+        : name(name), bindType(bindType), index(index)
+    {
+        LoadTexture(name);
+    }
+
+    Texture::~Texture()
+    {
+    }
+
+    void Texture::Bind()
+    {
+        // 예외 처리.
+        if (!textureData)
+        {
+            return;
+        }
+
+        // 컨텍스트 얻기.
+        static ID3D11DeviceContext& context = Engine::Get().Context();
+
+        // 바인딩.
+        if (bindType == BindType::VertexShader)
+        {
+            context.VSSetShaderResources(index, 1, &textureData->shaderResourceView);
+            context.VSSetSamplers(index, 1, &textureData->samplerState);
+        }
+        else if (bindType == BindType::PixelShader)
+        {
+            context.PSSetShaderResources(index, 1, &textureData->shaderResourceView);
+            context.PSSetSamplers(index, 1, &textureData->samplerState);
+        }
+    }
+
+    void Texture::LoadTexture(const std::string& name)
+    {
+        // 이미지 파일 로드.
+        // 경로 설정.
+        char path[256] = {};
+        sprintf_s(path, 256, "../Assets/Textures/%s", name.c_str());
+
+        // 객체 생성.
+        textureData = std::make_unique<TextureData>();
+
+        // 이미지 파일 로드.
+        textureData->data = 
+            stbi_load(path, &textureData->width, &textureData->height, &textureData->channelCount, 0);
+
+        // 예외 처리.
+        if (!textureData->data)
+        {
+            std::cout << "Failed to load texture file: " << name << '\n';
+            __debugbreak();
+        }
+    }
+}
--- DirectxEngine/Engine/main.cpp
+++ DirectxEngine/Engine/main.cpp
@@ -3,13 +3,12 @@
 #include <iostream>
 
 #include "Core/Engine.h"
+#include "Render/Texture.h"
 
 using namespace DirectxEngine;
 
 int main()
 {
-    std::cout << "테스트\n";
-
     Engine engine(1280, 800, TEXT("Engine Demo"), GetModuleHandle(nullptr));
     engine.Run();
 }
