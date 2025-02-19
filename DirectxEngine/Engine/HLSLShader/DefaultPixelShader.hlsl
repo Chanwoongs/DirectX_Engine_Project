66def24213d33b5d927b43b9d797e53820b8e03e
Chanwoong Bang <bck4044@gmail.com>
2025-02-19 15:13:08 +0900

    feat: 텍스처 매핑 용 DX 리소스 세팅

--- DirectxEngine/Engine/HLSLShader/DefaultPixelShader.hlsl
+++ DirectxEngine/Engine/HLSLShader/DefaultPixelShader.hlsl
@@ -5,8 +5,14 @@ struct PixelInput
     float2 texCoord : TEXCOORD;
 };
 
+// Texture.
+texture2D diffuseMap : register(t0);
+SamplerState diffuseSmapler : register(s0);
+
 float4 main(PixelInput input) : SV_TARGET
 {
-	//return float4(input.color, 1);
-	return float4(input.texCoord, 0.0f, 1.0f);
+    // Sampling.
+    float4 texColor = diffuseMap.Sample(diffuseSmapler, input.texCoord);
+    
+    return texColor;
 } 
